2953cddafcbf4fc4a2f8d75837ef5e6d14a6b4c1
gabrielBlankenburg <gabriel.blankenburg98@gmail.com>
2017-05-10 00:18:26 -0300

    Adicionado a função que para o slider e removido os botões prev e next

--- Slider-JS/javascript/script.js
+++ Slider-JS/javascript/script.js
@@ -1,68 +1,39 @@
+/* Cria um slider que ao apertar play empurra as imagens usando margem, as imagens são posicionadas dentro de uma div (criada pela função), as imagens
+ ficam lado a lado, porém a div só mostra o equivalente ao tamanho de uma foto. Quando uma foto some completamente do slider, ela é adicionada
+ ao final do slider novamente, e é assim infinitamente para usar esse slider basta criar uma div com o id 'simple-slider' e adicionar as imagens dentro
+  */
 function marginSlider(){
-    //Pega o ID do elemento e os elementos dentro dele
     var simpleSlider = document.getElementById('simple-slider');
     var img = Array.prototype.slice.call(simpleSlider.children);
-    //cria a div que permite os elementos ficarem lado a lado
     var slider = document.createElement('div');
     slider.className = 'slider';
-    //deixa a div no tamanho certo para todos os elementos
+    // Deixa a div no tamanho certo para todos os elementos
     slider.style.width = (simpleSlider.offsetWidth * img.length) + 'px';
     slider.style.height = simpleSlider.offsetHeight + 'px';
-    //impede as imagens que não devem aparecer de aparecerem
+    //impede os elementos que não devem aparecer de aparecerem
     slider.style.overflow = 'hidden';
     simpleSlider.style.overflow = 'hidden';
     slider.style.overflow = 'hidden';
     simpleSlider.style.position = 'relative';
 
-    //configura todos os botões
-    var setDefaultButtons = function(next, prev, play){
-        next.className = 'next';
-        prev.className = 'prev';
-        play.className = 'play';
-        var btn = [next, prev, play];
-        var setNextPrev = function(next, prev){
-            var both = [next, prev];
-            both.forEach(function(x){
-                x.style.top = '47%';
-                x.style.height = '7%';
-                x.style.width = '7%';
-                x.style.background = '#333';
-                x.style.borderRadius = '80%';
-                x.style.fontSize = '1.5em';
-                x.style.paddingBottom = '6%';
-                x.style.paddingRight = '6%';
-            });
-            next.style.right = '2%';
-            next.innerHTML = '&rarr;';
-
-            prev.style.left = '2%';
-            prev.innerHTML = '&larr;';
-            btn.forEach(function(x){
-                x.style.position = 'absolute';
-                x.style.zIndex = '1';
-                x.style.border = '1px solid rgba(20,20,20,.5)';
-                x.style.color = 'white';  
-                x.style.opacity = '0.5';
-                simpleSlider.appendChild(x); 
-            });
-        }
-
-        setNextPrev(next, prev);  
-        play.style.bottom = '3%';
-        play.style.background = '#111';
-        play.style.left = '45%';
-        play.style.width = '10%';
-        play.style.fontFamily = 'Impact, Charcoal, sans-serif';
-        play.innerHTML = 'PLAY'; 
-    };
-    //cria os botoes do slider
+    // O botão play
     var play = document.createElement('button');
-    var next = document.createElement('button');
-    var prev = document.createElement('button');
-
-    setDefaultButtons(next, prev, play);
+    play.className = 'play'; 
+    play.style.bottom = '3%';
+    play.style.background = '#111';
+    play.style.left = '45%';
+    play.style.width = '10%';
+    play.style.fontFamily = 'Impact, Charcoal, sans-serif';
+    play.innerHTML = 'PLAY';
+    play.style.position = 'absolute';
+    play.style.zIndex = '1';
+    play.style.border = '1px solid rgba(20,20,20,.5)';
+    play.style.color = 'white';  
+    play.style.opacity = '0.5';
+    simpleSlider.appendChild(play);
     
-    //configura as imagens
+    
+    // Configura os elementos e adiciona no slider
     var setImage = function(img){
         img.style.position = 'relative';
         img.style.top = '0';
@@ -71,44 +42,72 @@ function marginSlider(){
         img.style.float = 'left';
         img.style.margin = '0';
         img.style.border = 'none';
-        //adiciona as imagens no slider
         slider.appendChild(img);    
     };
     img.forEach(function(img){
         setImage(img);
     });
-    //adiciona a div criada em simpleSlider
+    // Adiciona a div criada em simpleSlider
     simpleSlider.appendChild(slider);
     document.body.appendChild(simpleSlider);
-    
-    //rola infinitamente as imagens ao clicar em play
-    var start = function(i){
-        var interval = setInterval(function(){
-            var stop = function(){
-                clearInterval(interval);
-            };
-            //quando a margem é do tamanho do width do slider reposiciona ela pro final
-            if(parseFloat(img[i].style.marginLeft) == - (simpleSlider.offsetWidth)){
-                slider.appendChild(img[i]);
-                setImage(img[i]);
-                stop();
-                if(i == img.length - 1){
-                    i = 0;
+
+    // Controla os elementos
+    var imgCount = 0;
+
+    // É aqui que o slider inicia
+    var startSlider = function(run){
+
+        //Se o slider for pausado no meio de uma transição de elementos, o slider mostra apenas um deles
+        play.addEventListener('click', function(){
+            run = false;
+            if(parseFloat(img[imgCount].style.marginLeft) != - (simpleSlider.offsetWidth) && parseFloat(img[imgCount].style.marginLeft) != 0){
+                if( - parseFloat(img[imgCount].style.marginLeft) < simpleSlider.offsetWidth / 1.5){
+                    img[imgCount].style.marginLeft = '0';
                 }
                 else{
-                    i++;
+                    img[imgCount].style.marginLeft = - simpleSlider.offsetWidth;
+                    slider.appendChild(img[imgCount]);
+                    setImage(img[imgCount]);
+                    if(imgCount == img.length - 1){
+                        imgCount = 0;
+                    }
+                    else{
+                        imgCount++;
+                    }
                 }
-                start(i);
             }
-            //puxa a próxima imagem usando margem
-            img[i].style.marginLeft = (parseFloat(img[i].style.marginLeft) - 1) + 'px';
+        });
+
+        // É aqui onde rola o slider
+        var interval = setInterval(function(){
+            if(run === true){
+                if(parseFloat(img[imgCount].style.marginLeft) == - (simpleSlider.offsetWidth)){
+                    slider.appendChild(img[imgCount]);
+                    setImage(img[imgCount]);
+                    if(imgCount == img.length - 1){
+                        imgCount = 0;
+                    }
+                    else{
+                        imgCount++;
+                    }
+                }
+                img[imgCount].style.marginLeft = (parseFloat(img[imgCount].style.marginLeft) - (simpleSlider.offsetWidth / 500)) + 'px';
+            }
+            else{
+                clearInterval(interval);
+            }
+
         }, 1);
     };
 
+    //configura o botão play
     play.addEventListener('click', function(){
         if(play.innerHTML == 'PLAY'){
-            start(0);
+            startSlider(true);
             play.innerHTML = 'STOP';
+        }
+        else{
+            play.innerHTML = 'PLAY';
         }
     });
     
